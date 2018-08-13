d5b381e0e0d6215e3a5d18a3103a65545e3e59e8
Ritumetse <ntshechengpogisho@gmail.com>
2018-08-13 16:22:14 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,74 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
+Users ={
 
 }
+   public form 			: FormGroup;  
+
+   constructor(public navCtrl 		: NavController,
+               public navParams 	: NavParams,
+               private _FB          : FormBuilder)
+   {
+
+      this.form = this._FB.group({
+        username:['',[Validators.required,Validators.minLength(8)]],
+        password:['',[Validators.required,Validators.maxLength(6)]],
+         name    : ['', Validators.required],
+
+         Skills     : this._FB.array([
+            this.initTechnologyFields()
+            
+         ])
+      });
+   }
+
+
+
+   initTechnologyFields() : FormGroup
+   {
+      return this._FB.group({
+         name 		: ['', Validators.required],
+         username		: ['', Validators.required],
+         password 	: ['', Validators.required],
+         
+
+         
+      });
+   }
+
+
+
+   addNewInputField() : void
+   {
+      const control = <FormArray>this.form.controls.Skills;
+      control.push(this.initTechnologyFields());
+   }
+
+   removeInputField(i : number) : void
+   {
+      const control = <FormArray>this.form.controls.Skills;
+      control.removeAt(i);
+   }
+
+   manage(val : any) : void
+   {
+      console.dir(val);
+   }
+  }
+
+  export interface User{
+    username:string;
+    password:string;
+    Skills:{
+  name :string;
+
+    }
+  }
